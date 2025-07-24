aca5e48ced7fd6762c887136bb60d6b841782eef
Narahari <narahari@Hari-MacBook-Air.local>
2025-07-24 10:57:12 +0530

    j

---
+++ open_mypal_1_to_15.bat
@@ -0,0 +1,13 @@
+@echo off
+set "MYPAL_PATH=C:\Program Files\Mypal\mypal.exe"
+set "FIRST_URL=https://ssotp3.online/"
+
+for /L %%i in (1,1,15) do (
+    set "PROFILE_NAME=%%i"
+    call :openBrowser "%%PROFILE_NAME%%"
+)
+goto :eof
+
+:openBrowser
+start "" "%MYPAL_PATH%" -no-remote -P %1 "%FIRST_URL%"
+goto :eof
