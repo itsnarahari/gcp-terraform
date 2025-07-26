1d5fcd97142f265d854248c2981a333d212f3ca3
Narahari <narahari@Hari-MacBook-Air.local>
2025-07-26 19:22:32 +0530

    sdf

---
+++ autoSelect.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         SSMMS Auto Picker Loader
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Load script from GitHub
+// @grant        none
+// @require      https://github.com/itsnarahari/gcp-terraform/blob/main/script.js
+// ==/UserScript==
