4bb1c91d11b690d09f68d46c155bb7ccdc27cd37
chinnanaik47 <chinnanaik47@gmail.com>
2025-07-26 23:16:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 (function() {
-document.getElementsByClassName('Dropdown')[0].value = 46;
-  PopulateGrid(45);
+document.getElementsByClassName('Dropdown')[0].value = 24;
+  PopulateGrid(24);
 
 var intId = setInterval(function(){    
 for(var i=0;i<30;i++){
     var data = document.getElementsByClassName("GridviewScrollItem")[i].cells[2].innerHTML;
-    if( data.indexOf("RamannagudemSBA(2025)") != -1) {        
+    if( data.indexOf("Veerapuram De-Siltation(2025)") != -1) {        
         $("input[type='radio']")[i].click();
         clearInterval(intId);
         break;
