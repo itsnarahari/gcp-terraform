21bee2a5493d321c1f66d3000b4b84cf2c0469e1
chinnanaik47 <chinnanaik47@gmail.com>
2025-07-26 23:21:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementsByClassName('Dropdown')[0].value = 24;
 var intId = setInterval(function(){    
 for(var i=0;i<30;i++){
     var data = document.getElementsByClassName("GridviewScrollItem")[i].cells[2].innerHTML;
-    if( data.indexOf("Veerapuram De-Siltation(2025)") != -1) {        
+    if( data.indexOf("GP Palli SBAB5(2025)") != -1) {        
         $("input[type='radio']")[i].click();
         clearInterval(intId);
         break;
