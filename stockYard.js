6ff6e4efc5f3733fa60aa382ab16f9adebfa96e8
chinnanaik47 <chinnanaik47@gmail.com>
2025-07-27 14:02:59 +0530

    Update stockYard.js

--- stockYard.js
+++ stockYard.js
@@ -2,9 +2,9 @@
   console.log(
     "Stock Yard script loaded successfully from GitHub and running..."
   );
-  document.getElementsByClassName("Dropdown")[0].value = 24;
-  PopulateGrid(24);
-  console.log("Dropdown value set to 24 and grid populated.");
+  document.getElementsByClassName("Dropdown")[0].value = 25;
+  PopulateGrid(25);
+  console.log("Dropdown value set to 25 and grid populated.");
 
   var intId = setInterval(function () {
     for (var i = 0; i < 30; i++) {
