4de596716dbe6a879bf52092872452b6c89f05a1
chinnanaik47 <chinnanaik47@gmail.com>
2025-07-27 14:17:35 +0530

    Update stockYard.js

--- stockYard.js
+++ stockYard.js
@@ -2,16 +2,16 @@
   console.log(
     "Stock Yard script loaded successfully from GitHub and running..."
   );
-  document.getElementsByClassName("Dropdown")[0].value = 25;
-  PopulateGrid(25);
-  console.log("Dropdown value set to 25 and grid populated.");
+  document.getElementsByClassName("Dropdown")[0].value = 24;
+  PopulateGrid(24);
+  console.log("Dropdown value set to 24 and grid populated.");
 
   var intId = setInterval(function () {
     for (var i = 0; i < 30; i++) {
       var data =
         document.getElementsByClassName("GridviewScrollItem")[i].cells[2]
           .innerHTML;
-      if (data.indexOf("Chintakunta Desiltation(2025)") != -1) {
+      if (data.indexOf("Chinna") != -1) {
         $("input[type='radio']")[i].click();
         clearInterval(intId);
         break;
